ea9d3b01ab824679df42281e743c22e3809a5afa
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 00:35:58 +0100

    Export app

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+// Your first API endpoint
+
+module.exports = app;
