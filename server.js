fee7c0bc3acabf1973a9760a8bc9b3c220b95175
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 12:45:53 +0100

    Connect to DB

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
 dotenv.config();
 
 const app = require("./app");
+
+const DB = process.env.MONGODB_URI;
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("Database successfully connected"))
+  .catch((err) => {
+    console.error(err);
+    process.exit();
+  });
 
 const port = process.env.PORT || 3000;
 
