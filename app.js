8e6b21fbcff97163a422a9d4bbba638f4fbd91ee
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 14:22:00 +0100

    Import url router and refactor

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const dns = require("node:dns");
-const ShortUniqueId = require("short-unique-id");
+const urlRouter = require("./routes/urlRoutes");
+
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
+// Middleware
 app.use(cors());
 
 // Body parsing middleware
@@ -19,35 +17,6 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-const options = {
-  family: 6,
-  hints: dns.ADDRCONFIG | dns.V4MAPPED,
-};
-
-app.post("/api/shorturl", (req, res) => {
-  const { url } = req.body;
-
-  // Remove protocol and characters after (and including) trailing /
-  const formattedUrl = url
-    .replace(/(^\w+:|^)\/\//, "")
-    .replace(/\/[^\/]*$/, "");
-
-  dns.lookup(formattedUrl, options, (err, address, family) => {
-    if (err) {
-      return res.json({
-        error: "Invalid url",
-      });
-    } else {
-      const { randomUUID } = new ShortUniqueId({ length: 5 });
-
-      const result = {
-        original_url: url,
-        short_url: randomUUID(),
-      };
-
-      return res.json(result);
-    }
-  });
-});
+app.use("/api/shorturl", urlRouter);
 
 module.exports = app;
